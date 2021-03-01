97e58628dcb3566d68ed7f4e91cf77eeaf3b2339
Jan Thomä <janthomae@janthomae.de>
2021-03-01 23:20:20 +0100

    feature: add build scripts

--- build.cmd
+++ build.cmd
@@ -1,1 +1,3 @@
-java -jar antlr-4.9.1-complete.jar -o gen -visitor -Dlanguage=Python3 tscn_lexer.g4 tscn_parser.g4
+@echo off
+java -jar antlr-4.9.1-complete.jar -o gen -visitor -Dlanguage=Python3 grammar/tscn_lexer.g4 grammar/tscn_parser.g4
+python -m pipenv run pyinstaller -F tscnmerge.py
